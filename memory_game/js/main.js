5110a9c4631373af6ece485bb90014732118a0ba
Brian Brown <BrianKeith@Brians-MacBook-Pro.local>
2020-01-16 16:56:31 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,18 +1,19 @@
 const cards = ["queen", "queen", "king", "king"]
 
 const cardsInPlay = []
-let cardOne = cards[2];
-cardsInPlay.push(cardOne);
-
-console.log("User Flipped " + cardOne);
-
-let cardTwo = cards[3];
-cardsInPlay.push(cardTwo);
-
-console.log("User Flipped " + cardTwo);
-
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-	result = alert("You found a match!");
-} else if (cardsInPlay.length ===2 && cardsInPlay[0] !== cardsInPlay[1]) {
-	result = alert("Sorry, try again.");
+function checkForMatch() {
+	if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+	console.log("You found a match!");
+} else if (cardsInPlay.length === 2 && cardsInPlay[0] !== cardsInPlay[1]) {
+	console.log("Sorry, try again.");
 }
+};
+function flipCard(cardId) {
+ console.log("User flipped " + cards[cardId]);
+ cardsInPlay.push(cards[cardId])
+
+checkForMatch();
+};
+flipCard(0);
+flipCard(2);
+
